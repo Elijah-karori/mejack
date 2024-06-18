08826993edba3b9296e530f3ae5e55701be5985f
Elijah Karori Orito <elijakarori23@gmail.com>
2024-06-18 06:14:13 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/mejack",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
                    };
